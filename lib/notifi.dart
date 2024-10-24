ba805f074aee7e5af991d3d419c4f2e0fe8f8e76
Adam Crow <acrow@sala.local>
2024-10-24 21:40:24 +1100

    Added more logging for status

--- lib/notifi.dart
+++ lib/notifi.dart
@@ -115,6 +115,13 @@ class Notifi {
       sound: true,
     );
 
+if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+  print('User granted permission');
+} else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+  print('User granted provisional permission');
+} else {
+  print('User declined or has not accepted permission');
+}
     print('User granted permission: ${settings.authorizationStatus}');
 
     FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) async {
