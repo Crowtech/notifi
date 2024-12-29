5df24659725bda26bbcc929d81515228c0545921
Adam Crow <acrow@crowtech.com.au>
2024-12-29 17:07:34 +1100

    Updated i18n

--- generate_translations.sh
+++ generate_translations.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dart run slang_gpt --target=$1 --api-key=sk-hpRACerb0gtv-_QA-pVSMmAAlw3YQYFMvzVYen89R_T3BlbkFJrO9kHHWWy-twCmbZwQiFiiou1ObuOWkLzobeTuhogA
+#dart run slang_gpt --target=$1 --api-key=
