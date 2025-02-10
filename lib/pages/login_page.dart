6e7faecc24009ab8cbb39e7809d4848d1ac39faf
Adam Crow <acrow@crowtech.com.au>
2025-02-10 19:32:34 +1100

    Updated

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:notifi/jwt_utils.dart';
+import 'package:notifi/notifi.dart';
 import 'package:oidc/oidc.dart';
 
 import '../state/auth_controller.dart';
 import '../widgets/action_button.dart';
+import 'package:notifi/app_state.dart' as app_state;
+import 'package:logger/logger.dart' as logger;
+import 'package:provider/provider.dart' as prov;
+
+var log = logger.Logger(
+  printer: logger.PrettyPrinter(),
+  level: logger.Level.info,
+);
+
+var logNoStack = logger.Logger(
+  printer: logger.PrettyPrinter(methodCount: 0),
+  level: logger.Level.info,
+);
 
 class LoginPage extends HookConsumerWidget {
   LoginPage({super.key});
@@ -50,6 +67,46 @@ class LoginPage extends HookConsumerWidget {
               'myPassword',
             );
 
+      Future<void> login3() async {
+
+    final currentRoute = GoRouterState.of(context);
+    final originalUri =
+        currentRoute.uri.queryParameters[OidcConstants_Store.originalUri];
+    final parsedOriginalUri =
+        originalUri == null ? null : Uri.tryParse(originalUri);
+    try {
+      OidcUser? result;
+    
+        result = await app_state.currentManager.loginAuthorizationCodeFlow(
+          originalUri: parsedOriginalUri ?? Uri.parse('/'),
+          //store any arbitrary data, here we store the authorization
+          //start time.
+          extraStateData: DateTime.now().toIso8601String(),
+          options: _getOptions(),
+          //NOTE: you can pass more parameters here.
+        );
+        if (result != null) {
+          //ref.read(currentUserProvider.notifier).setOidc(result);
+            logNoStack
+          .i("gloginPage accessToken = ${getAccessToken(result)}");
+        } else {
+            logNoStack
+          .i("loginPage accessToken is null");
+        }
+   
+  
+      if (kIsWeb &&
+          webNavigationMode ==
+              OidcPlatformSpecificOptions_Web_NavigationMode.samePage) {
+        //in samePage navigation, you can't know the result here.
+        return;
+      } 
+    } catch (e) {
+      app_state.log.e(e.toString());
+    }
+  }
+
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -57,7 +114,7 @@ class LoginPage extends HookConsumerWidget {
           children: [
             const Text('Login Page'),
             ActionButton(
-              onPressed: login2,
+              onPressed: login3,
               icon: const SizedBox.shrink(),
               label: const Text('Login'),
             ),
