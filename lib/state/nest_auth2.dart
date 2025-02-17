6ce27084bdb3524d53291d3644c788eecc97946a
Adam Crow <acrow@crowtech.com.au>
2025-02-17 15:23:58 +1100

    NestAuth notifier update

---
+++ lib/state/nest_auth2.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NestAuthController with ChangeNotifier {
+
+//Within this section, you can integrate authentication methods 
+//such as Firebase, SharedPreferences, and more. 
+
+ bool isLoggedIn = false;
+
+  void signIn() {
+    isLoggedIn = true;
+    notifyListeners();
+  }
+
+  void signOut() {
+    isLoggedIn = false;
+    notifyListeners();
+  }
+}
+
+final nestAuthProvider = ChangeNotifierProvider((ref) => NestAuthController());
