8cd78224e03ac44e90d5e809f5a972fc19dc8adc
Adam Crow <acrow@crowtech.com.au>
2025-02-19 20:37:00 +1100

    Added test provider

---
+++ lib/riverpod/nest_filter_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'nest_filter_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$nestFilterProviderHash() =>
+    r'8a3c0a574c16acf60cb1be538c7e434147a1d271';
+
+/// See also [NestFilterProvider].
+@ProviderFor(NestFilterProvider)
+final nestFilterProviderProvider =
+    AutoDisposeNotifierProvider<NestFilterProvider, NestFilter>.internal(
+  NestFilterProvider.new,
+  name: r'nestFilterProviderProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$nestFilterProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$NestFilterProvider = AutoDisposeNotifier<NestFilter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
