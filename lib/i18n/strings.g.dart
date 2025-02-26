fdc47591dad3ab4ebdbf3e5feafa51feb417b02f
Adam Crow <acrow@crowtech.com.au>
2025-02-26 17:28:45 +1100

    Updated slang

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 146 (73 per locale)
 ///
-/// Built on 2025-02-26 at 06:20 UTC
+/// Built on 2025-02-26 at 06:28 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
