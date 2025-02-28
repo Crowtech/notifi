eca2b254c62253a8de526d826fd38eaca9a189ea
Adam Crow <acrow@crowtech.com.au>
2025-02-28 17:08:02 +1100

    Fixed WebIdentityToken

--- lib/models/minio_token_request.dart
+++ lib/models/minio_token_request.dart
@@ -12,16 +12,12 @@ class MinioTokenRequest {
   static String className = "MinioTokenRequest";
   static String tablename = className.toLowerCase();
 
-
-  String Action='AssumeRoleWithWebIdentity';
+  String Action = 'AssumeRoleWithWebIdentity';
   String Version = '2011-06-15';
-  int Duration = 86000;
-  String WebidentityToken;
+  int DurationSeconds = 86000;
+  String WebIdentityToken;
 
-
-  MinioTokenRequest(
-      {required this.WebidentityToken
-});
+  MinioTokenRequest({required this.WebIdentityToken});
 
   factory MinioTokenRequest.fromJson(Map<String, dynamic> json) =>
       _$MinioTokenRequestFromJson(json);
@@ -34,17 +30,14 @@ class MinioTokenRequest {
 
   @override
   String toString() {
-    return "MinioTokenRequest:$Action $Version $Duration $WebidentityToken";
+    return "MinioTokenRequest:$Action $Version $DurationSeconds $WebIdentityToken";
   }
 
   MinioTokenRequest copyWith({
     final String? query,
   }) {
     return MinioTokenRequest(
-      WebidentityToken: WebidentityToken ?? this.WebidentityToken,
+      WebIdentityToken: WebIdentityToken ?? this.WebIdentityToken,
     );
   }
-
- 
 }
-
--- lib/models/minio_token_request.g.dart
+++ lib/models/minio_token_request.g.dart
@@ -8,16 +8,16 @@ part of 'minio_token_request.dart';
 
 MinioTokenRequest _$MinioTokenRequestFromJson(Map<String, dynamic> json) =>
     MinioTokenRequest(
-      WebidentityToken: json['WebidentityToken'] as String,
+      WebIdentityToken: json['WebIdentityToken'] as String,
     )
       ..Action = json['Action'] as String
       ..Version = json['Version'] as String
-      ..Duration = (json['Duration'] as num).toInt();
+      ..DurationSeconds = (json['DurationSeconds'] as num).toInt();
 
 Map<String, dynamic> _$MinioTokenRequestToJson(MinioTokenRequest instance) =>
     <String, dynamic>{
       'Action': instance.Action,
       'Version': instance.Version,
-      'Duration': instance.Duration,
-      'WebidentityToken': instance.WebidentityToken,
+      'DurationSeconds': instance.DurationSeconds,
+      'WebIdentityToken': instance.WebIdentityToken,
     };
