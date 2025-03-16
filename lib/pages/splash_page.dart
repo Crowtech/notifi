f61446abb14624154591e6429994a95bd6606663
Adam Crow <acrow@crowtech.com.au>
2025-03-16 18:33:51 +1100

    Updated slang

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -4,16 +4,17 @@ import 'package:flutter/material.dart';
 import '../widgets/loading_spinner.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+  SplashPage(String this.splashText,{super.key});
+  String splashText;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Splash Page'),
+            Text(splashText),
             SizedBox(height: 16),
             LoadingSpinner(),
           ],
