429f799ab7598c8c3506e37ab4f971dafeb9ff56
Adam Crow <acrow@crowtech.com.au>
2025-03-16 18:02:46 +1100

    Updated slang

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 218 (109 per locale)
 ///
-/// Built on 2025-03-16 at 06:55 UTC
+/// Built on 2025-03-16 at 07:02 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
--- lib/routing/not_found_screen.dart
+++ lib/routing/not_found_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notifi/common_widgets/empty_placeholder_widget.dart';
-import 'nest_router.dart';
 import 'package:notifi/i18n/strings.g.dart' as nt;
 
 /// Simple not found screen used for 404 errors (page not found on web)
