c8a18d8856686f6b0004f95c45e84e7c893fed47
Adam Crow <acrow@crowtech.com.au>
2025-03-19 15:31:56 +1100

    Updated slang

---
+++ lib/organizations/src/features/organizations/presentation/organizations/selected_organizations.dart
@@ -0,0 +1,50 @@
+import 'package:notifi/models/nest_filter_type.dart';
+import 'package:notifi/models/nestfilter.dart';
+import 'package:notifi/models/organization.dart';
+import 'package:notifi/models/person.dart';
+import 'package:notifi/state/nest_auth2.dart';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:logger/logger.dart' as logger;
+import 'package:latlong2/latlong.dart';
+
+part 'selected_organizations.g.dart';
+
+var log = logger.Logger(
+  printer: logger.PrettyPrinter(),
+  level: logger.Level.info,
+);
+
+var logNoStack = logger.Logger(
+  printer: logger.PrettyPrinter(methodCount: 0),
+  level: logger.Level.info,
+);
+
+@Riverpod(keepAlive: true)
+class SelectedOrganizations extends _$SelectedOrganizations {
+   
+  @override
+  List<Organization> build() {
+    logNoStack.i("SELECTED ORGS BUILD");
+    return  [];
+  }
+
+  void addOrg(Organization organization) {
+    logNoStack.i("SELECTED_ORGS: add ${organization.url}");
+    state = [...state, organization];
+  }
+
+  void removeOrg(Organization organization) {
+        logNoStack.i("SELECTED_ORGS: remove ${organization.url}");
+    state = [...state]..remove(organization);
+  }
+
+  void update(Organization organization, bool add) {
+    if (add == true) {
+      addOrg(organization);
+    } else {
+      removeOrg(organization);
+    }
+    
+  }
+}
