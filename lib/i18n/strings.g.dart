6a1ab7c03e7e166bfd8524307df04ddcd2a8c3b4
Adam Crow <acrow@crowtech.com.au>
2025-03-19 15:32:33 +1100

    Updated slang

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 248 (124 per locale)
 ///
-/// Built on 2025-03-19 at 04:21 UTC
+/// Built on 2025-03-19 at 04:32 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
---
+++ lib/organizations/src/features/organizations/presentation/organizations/selected_organizations.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'selected_organizations.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$selectedOrganizationsHash() =>
+    r'a4459a7e2f45f1a832d5769e9550e8dc3c281a8a';
+
+/// See also [SelectedOrganizations].
+@ProviderFor(SelectedOrganizations)
+final selectedOrganizationsProvider =
+    NotifierProvider<SelectedOrganizations, List<Organization>>.internal(
+  SelectedOrganizations.new,
+  name: r'selectedOrganizationsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$selectedOrganizationsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SelectedOrganizations = Notifier<List<Organization>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
