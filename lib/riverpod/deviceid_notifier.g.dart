f5de38f73df80931d251a79ee1d812277008c753
Adam Crow <acrow@crowtech.com.au>
2025-03-25 09:06:07 +1100

    Updated deviceId

---
+++ lib/riverpod/deviceid_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'deviceid_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$deviceIdNotifierHash() => r'ee6f0eead077be53d74bf35d241ba1cfdbbba5c7';
+
+/// See also [DeviceIdNotifier].
+@ProviderFor(DeviceIdNotifier)
+final deviceIdNotifierProvider =
+    NotifierProvider<DeviceIdNotifier, String>.internal(
+  DeviceIdNotifier.new,
+  name: r'deviceIdNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$deviceIdNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DeviceIdNotifier = Notifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
