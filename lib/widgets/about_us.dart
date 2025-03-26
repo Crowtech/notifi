d92451f5b4f4eaa12ed941b97813376fc1a84fc3
Adam Crow <acrow@crowtech.com.au>
2025-03-26 14:46:29 +1100

    Added install time to about us

--- lib/widgets/about_us.dart
+++ lib/widgets/about_us.dart
@@ -19,6 +19,7 @@ Future<void> AboutUsDialog(BuildContext context, String appTitle, Widget logo)  
           content:  Text(
             '$appTitle\n'
             'Version\t${packageInfo.version}\n'
+            'Installed\t${packageInfo.installTime}\n'
             'Device\t${deviceId}\n'
             '\n'
             ' © Crowtech 2024,2025 under license to  ${defaultRealm.capitalise()}' ,
