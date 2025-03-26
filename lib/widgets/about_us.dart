46e9cc6d895fcc8a3bdf2a32a820d9b63ae748ab
Adam Crow <acrow@crowtech.com.au>
2025-03-26 12:38:25 +1100

    Updated packageInfo

--- lib/widgets/about_us.dart
+++ lib/widgets/about_us.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:notifi/credentials.dart';
-import 'package:notifi/jwt_utils.dart';
 import 'package:notifi/i18n/strings.g.dart' as nt;
-import 'package:notifi/notifi.dart';
-import 'package:provider/provider.dart' as prov;
+import 'package:notifi/jwt_utils.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 
-Future<void> AboutUsDialog(BuildContext context, String appTitle) {
+Future<void> AboutUsDialog(BuildContext context, String appTitle)  async{
+    PackageInfo packageInfo = await fetchPackageInfo();
 
+    
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
@@ -15,7 +16,7 @@ Future<void> AboutUsDialog(BuildContext context, String appTitle) {
           title:  Text('About ${appTitle}'),
           content:  Text(
             '$appTitle\n'
-            'Version ${prov.Provider.of<Notifi>(context, listen: false).packageInfo!.version}\n'
+            'Version ${packageInfo.version}\n'
             '\n'
             ' © ${defaultRealm.capitalise()} 2024,2025 and © Crowtech 2024,2025',
           ),
