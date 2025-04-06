fa4e4e3c824419f3aece8715a95454a9565346d4
Adam Crow <acrow@crowtech.com.au>
2025-04-06 19:30:58 +1000

    Updated notifi to help dev

--- lib/riverpod/fcm_notifier.dart
+++ lib/riverpod/fcm_notifier.dart
@@ -1,7 +1,10 @@
 import 'dart:ui';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notifi/api_utils.dart';
+import 'package:notifi/credentials.dart';
 import 'package:notifi/jwt_utils.dart';
 import 'package:notifi/state/nest_auth2.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -22,34 +25,72 @@ var logNoStack = logger.Logger(
 
 @Riverpod(keepAlive: true)
 class FcmNotifier extends _$FcmNotifier {
+  List<String> topics = [
+    defaultRealm,
+    'test',
+  ];
+
   @override
-  String  build() {
+  String build() {
     return "NOT_READY";
   }
 
   void setFcm(String fcm) {
-      logNoStack.i("FCM_NOTIFIER: Setting fcm : $fcm}");
-   //   ref.read(sendFcmProvider(fcm));
+    logNoStack.i("FCM_NOTIFIER: Setting fcm : $fcm}");
+    //   ref.read(sendFcmProvider(fcm));
     state = fcm;
-   // 
+    //
   }
 
+  List<String> getTopics() {
+    return topics;
+  }
 
+  List<String> addTopic(String topic) {
+    topics.add(topic);
+    return topics;
+  }
+
+  List<String> removeTopic(String topic) {
+    topics.remove(topic);
+    return topics;
+  }
+
+  void setTopics() {
+    subscribeToTopics(topics);
+  }
 }
 
+void subscribeToTopics(List<String> topics) {
+  if (enableNotifications) {
+    if (!kIsWeb) {
+      logNoStack.i("NOTIFI2: Subscribing to topics");
+
+      for (final topic in topics) {
+        try {
+          FirebaseMessaging.instance.subscribeToTopic(topic).then((_) {
+            logNoStack.i("NOTIFI2: Subscribed to topic: $topic");
+          });
+        } on Exception catch (_) {
+          log.e("NOTIFI2: Firebase error");
+        }
+      }
+    } else {
+      logNoStack.i("NOTIFI2: Not subscribing to topics");
+    }
+  }
+}
 
 @Riverpod(keepAlive: true)
 void sendFcm(Ref ref) async {
-
   var fcm = ref.watch(fcmNotifierProvider);
-    String? token = ref.read(nestAuthProvider.notifier).token;
-    if (token == null) {
-      return;
-    }
-    logNoStack.i("SEND_FCM: Sending fcm to api : $fcm}");
-    String devicecode = await fetchDeviceId();
-    //Locale locale = (Locale)null;
-    Map result = await registerFCM( token, devicecode, fcm);
-    logNoStack.i("SEND_FCM: result = $result");
+  String? token = ref.read(nestAuthProvider.notifier).token;
+  if (token == null) {
+    return;
   }
-
+  logNoStack.i("SEND_FCM: Sending fcm to api : $fcm}");
+  String devicecode = await fetchDeviceId();
+  //Locale locale = (Locale)null;
+  Map result = await registerFCM(token, devicecode, fcm);
+  logNoStack.i("SEND_FCM: result = $result");
+}
--- lib/riverpod/fcm_notifier.g.dart
+++ lib/riverpod/fcm_notifier.g.dart
@@ -22,7 +22,7 @@ final sendFcmProvider = Provider<void>.internal(
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef SendFcmRef = ProviderRef<void>;
-String _$fcmNotifierHash() => r'8459c4fba7d5074558e0321149fd7e187a94913a';
+String _$fcmNotifierHash() => r'ed3805cae9993bc848806feaff2aadd690f71692';
 
 /// See also [FcmNotifier].
 @ProviderFor(FcmNotifier)
