a3c0f861bbf107ff435fcfe8b860af5a7bcb5227
Adam Crow <acrow@crowtech.com.au>
2025-04-07 14:21:02 +1000

    Updated notifi to help dev

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 256 (128 per locale)
 ///
-/// Built on 2025-04-07 at 03:03 UTC
+/// Built on 2025-04-07 at 04:20 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
