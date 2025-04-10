35ff58601cd4232ef888f03fa2d38da931c12090
Adam Crow <acrow@crowtech.com.au>
2025-04-10 14:58:28 +1000

    Updated org and person form page

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 292 (146 per locale)
 ///
-/// Built on 2025-04-10 at 04:56 UTC
+/// Built on 2025-04-10 at 04:58 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
