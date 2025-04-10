59960827d8c30b1b2827085db0f00641e8fe00dc
Adam Crow <acrow@crowtech.com.au>
2025-04-10 14:56:53 +1000

    Updated organizxation form

--- lib/i18n/strings_en.g.dart
+++ lib/i18n/strings_en.g.dart
@@ -41,9 +41,9 @@ class Translations implements BaseTranslations<AppLocale, Translations> {
 	// Translations
 	String get app_title => 'Crowtech';
 	String get notfound_404 => '404 - Page not found!';
-	late final TranslationsMenuEn menu = TranslationsMenuEn._(_root);
+	late final TranslationsMenuEn menu = TranslationsMenuEn.internal(_root);
 	String get about_us => 'About Us';
-	late final TranslationsAccountEn account = TranslationsAccountEn._(_root);
+	late final TranslationsAccountEn account = TranslationsAccountEn.internal(_root);
 	String get activity => 'Activity';
 	String get audio_access_denied => 'You have denied audio access';
 	String get audio_access_denied_without_prompt => 'Please go to Settings app to enable audio access';
@@ -97,25 +97,25 @@ class Translations implements BaseTranslations<AppLocale, Translations> {
 	String get moving => 'Moving';
 	String get odometer => 'Odometer';
 	String get openstreetmap => 'OpenStreetMap contributors';
-	late final TranslationsOrganizationEn organization = TranslationsOrganizationEn._(_root);
-	late final TranslationsResourceEn resource = TranslationsResourceEn._(_root);
-	late final TranslationsResourcesEn resources = TranslationsResourcesEn._(_root);
-	late final TranslationsResponseEn response = TranslationsResponseEn._(_root);
-	late final TranslationsSelectEn select = TranslationsSelectEn._(_root);
+	late final TranslationsOrganizationEn organization = TranslationsOrganizationEn.internal(_root);
+	late final TranslationsResourceEn resource = TranslationsResourceEn.internal(_root);
+	late final TranslationsResourcesEn resources = TranslationsResourcesEn.internal(_root);
+	late final TranslationsResponseEn response = TranslationsResponseEn.internal(_root);
+	late final TranslationsSelectEn select = TranslationsSelectEn.internal(_root);
 	String get privacy_policy => 'Privacy Policy';
 	String get profile => 'Profile';
 	String get pulltorefresh => 'Pull to refresh';
 	String get reset_offset => 'Reset Offset';
 	String get resetting_exposure_point => 'Resetting exposure point';
 	String get resetting_focus_point => 'Resetting focus point';
-	late final TranslationsSearchEn search = TranslationsSearchEn._(_root);
+	late final TranslationsSearchEn search = TranslationsSearchEn.internal(_root);
 	String get settings => 'Settings';
 	String get skip => 'Skip';
 	String get splash_text => 'Splash Screen';
 	String get still => 'Still';
 	String get terms_and_conditions => 'Terms & Conditions';
 	String get test_page => 'Test Page';
-	late final TranslationsUnknownEn unknown = TranslationsUnknownEn._(_root);
+	late final TranslationsUnknownEn unknown = TranslationsUnknownEn.internal(_root);
 	String get update_account => 'Update Account';
 	String get zoom_in => 'Zoom In';
 	String get zoom_out => 'Zoom out';
@@ -127,7 +127,7 @@ class Translations implements BaseTranslations<AppLocale, Translations> {
 
 // Path: menu
 class TranslationsMenuEn {
-	TranslationsMenuEn._(this._root);
+	TranslationsMenuEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -138,7 +138,7 @@ class TranslationsMenuEn {
 
 // Path: account
 class TranslationsAccountEn {
-	TranslationsAccountEn._(this._root);
+	TranslationsAccountEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -159,7 +159,7 @@ class TranslationsAccountEn {
 
 // Path: organization
 class TranslationsOrganizationEn {
-	TranslationsOrganizationEn._(this._root);
+	TranslationsOrganizationEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -180,7 +180,7 @@ class TranslationsOrganizationEn {
 
 // Path: resource
 class TranslationsResourceEn {
-	TranslationsResourceEn._(this._root);
+	TranslationsResourceEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -196,7 +196,7 @@ class TranslationsResourceEn {
 
 // Path: resources
 class TranslationsResourcesEn {
-	TranslationsResourcesEn._(this._root);
+	TranslationsResourcesEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -212,7 +212,7 @@ class TranslationsResourcesEn {
 
 // Path: response
 class TranslationsResponseEn {
-	TranslationsResponseEn._(this._root);
+	TranslationsResponseEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -232,7 +232,7 @@ class TranslationsResponseEn {
 
 // Path: select
 class TranslationsSelectEn {
-	TranslationsSelectEn._(this._root);
+	TranslationsSelectEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -254,7 +254,7 @@ class TranslationsSelectEn {
 
 // Path: search
 class TranslationsSearchEn {
-	TranslationsSearchEn._(this._root);
+	TranslationsSearchEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
@@ -270,7 +270,7 @@ class TranslationsSearchEn {
 
 // Path: unknown
 class TranslationsUnknownEn {
-	TranslationsUnknownEn._(this._root);
+	TranslationsUnknownEn.internal(this._root);
 
 	final Translations _root; // ignore: unused_field
 
--- lib/i18n/strings_zh.g.dart
+++ lib/i18n/strings_zh.g.dart
@@ -10,7 +10,7 @@ import 'package:slang/generated.dart';
 import 'strings.g.dart';
 
 // Path: <root>
-class TranslationsZh implements Translations {
+class TranslationsZh extends Translations {
 	/// You can call this constructor and build your own translation instance of this locale.
 	/// Constructing via the enum [AppLocale.build] is preferred.
 	TranslationsZh({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver, TranslationMetadata<AppLocale, Translations>? meta})
@@ -20,7 +20,9 @@ class TranslationsZh implements Translations {
 		    overrides: overrides ?? {},
 		    cardinalResolver: cardinalResolver,
 		    ordinalResolver: ordinalResolver,
-		  ) {
+		  ),
+		  super(cardinalResolver: cardinalResolver, ordinalResolver: ordinalResolver) {
+		super.$meta.setFlatMapFunction($meta.getTranslation); // copy base translations to super.$meta
 		$meta.setFlatMapFunction(_flatMapFunction);
 	}
 
@@ -28,7 +30,7 @@ class TranslationsZh implements Translations {
 	@override final TranslationMetadata<AppLocale, Translations> $meta;
 
 	/// Access flat map
-	@override dynamic operator[](String key) => $meta.getTranslation(key);
+	@override dynamic operator[](String key) => $meta.getTranslation(key) ?? super.$meta.getTranslation(key);
 
 	late final TranslationsZh _root = this; // ignore: unused_field
 
@@ -122,8 +124,8 @@ class TranslationsZh implements Translations {
 }
 
 // Path: menu
-class _TranslationsMenuZh implements TranslationsMenuEn {
-	_TranslationsMenuZh._(this._root);
+class _TranslationsMenuZh extends TranslationsMenuEn {
+	_TranslationsMenuZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
@@ -133,8 +135,8 @@ class _TranslationsMenuZh implements TranslationsMenuEn {
 }
 
 // Path: account
-class _TranslationsAccountZh implements TranslationsAccountEn {
-	_TranslationsAccountZh._(this._root);
+class _TranslationsAccountZh extends TranslationsAccountEn {
+	_TranslationsAccountZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
@@ -154,8 +156,8 @@ class _TranslationsAccountZh implements TranslationsAccountEn {
 }
 
 // Path: resource
-class _TranslationsResourceZh implements TranslationsResourceEn {
-	_TranslationsResourceZh._(this._root);
+class _TranslationsResourceZh extends TranslationsResourceEn {
+	_TranslationsResourceZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
@@ -170,8 +172,8 @@ class _TranslationsResourceZh implements TranslationsResourceEn {
 }
 
 // Path: resources
-class _TranslationsResourcesZh implements TranslationsResourcesEn {
-	_TranslationsResourcesZh._(this._root);
+class _TranslationsResourcesZh extends TranslationsResourcesEn {
+	_TranslationsResourcesZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
@@ -186,8 +188,8 @@ class _TranslationsResourcesZh implements TranslationsResourcesEn {
 }
 
 // Path: response
-class _TranslationsResponseZh implements TranslationsResponseEn {
-	_TranslationsResponseZh._(this._root);
+class _TranslationsResponseZh extends TranslationsResponseEn {
+	_TranslationsResponseZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
@@ -206,8 +208,8 @@ class _TranslationsResponseZh implements TranslationsResponseEn {
 }
 
 // Path: select
-class _TranslationsSelectZh implements TranslationsSelectEn {
-	_TranslationsSelectZh._(this._root);
+class _TranslationsSelectZh extends TranslationsSelectEn {
+	_TranslationsSelectZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
@@ -228,8 +230,8 @@ class _TranslationsSelectZh implements TranslationsSelectEn {
 }
 
 // Path: search
-class _TranslationsSearchZh implements TranslationsSearchEn {
-	_TranslationsSearchZh._(this._root);
+class _TranslationsSearchZh extends TranslationsSearchEn {
+	_TranslationsSearchZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
@@ -244,8 +246,8 @@ class _TranslationsSearchZh implements TranslationsSearchEn {
 }
 
 // Path: unknown
-class _TranslationsUnknownZh implements TranslationsUnknownEn {
-	_TranslationsUnknownZh._(this._root);
+class _TranslationsUnknownZh extends TranslationsUnknownEn {
+	_TranslationsUnknownZh._(TranslationsZh root) : this._root = root, super.internal(root);
 
 	final TranslationsZh _root; // ignore: unused_field
 
