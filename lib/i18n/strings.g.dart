c7a6deb0db23cef25e04aab2b32a0c64bc343b5b
Adam Crow <acrow@crowtech.com.au>
2025-05-03 13:00:04 +1000

    Updated nest

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 408 (204 per locale)
 ///
-/// Built on 2025-05-03 at 02:03 UTC
+/// Built on 2025-05-03 at 03:00 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
--- lib/pages/html_text_editor.dart
+++ lib/pages/html_text_editor.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:fleather/fleather.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 //import 'package:html_editor_enhanced/html_editor.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'package:notifi/i18n/strings.g.dart' as nt;
 //import 'package:web/web.dart' as web;
 
@@ -39,7 +42,10 @@ class HtmlTextEditor extends ConsumerStatefulWidget {
 class _HtmlTextEditorState extends ConsumerState<HtmlTextEditor> {
   final GlobalKey<FormFieldState> itemFormFieldKey =
       GlobalKey<FormFieldState>();
-
+  final FocusNode _focusNode = FocusNode();
+  final GlobalKey<EditorState> _editorKey = GlobalKey();
+       FleatherController? _controller;
+//late AppFlowyEditor editor;
 //  late HtmlEditorController controller;
 
   ///[customToolBarList] pass the custom toolbarList to show only selected styles in the editor
@@ -73,12 +79,51 @@ class _HtmlTextEditorState extends ConsumerState<HtmlTextEditor> {
   @override
   void initState() {
   //  controller = HtmlEditorController();
+   if (defaultTargetPlatform == TargetPlatform.android) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: []);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(systemNavigationBarColor: Colors.transparent),
+    );
+  }
+  //final json = jsonDecode('YOUR INPUT JSON STRING');
+  //final editorState = EditorState.blank(withInitialText: true);
+//final editorState = EditorState(document: Document.fromJson(json));
+// editor = AppFlowyEditor(
+//   editorState: editorState,
+//);
     super.initState();
+        if (kIsWeb) BrowserContextMenu.disableContextMenu();
+    _initController();
   }
 
   @override
   void dispose() {
     super.dispose();
+     if (kIsWeb) BrowserContextMenu.enableContextMenu();
+  }
+
+    Future<void> _initController() async {
+    try {
+      final result = await rootBundle.loadString('assets/welcome.json');
+      final heuristics = ParchmentHeuristics(
+        formatRules: [],
+        insertRules: [
+          ForceNewlineForInsertsAroundInlineImageRule(),
+        ],
+        deleteRules: [],
+      ).merge(ParchmentHeuristics.fallback);
+      final doc = ParchmentDocument.fromJson(
+        jsonDecode(result),
+        heuristics: heuristics,
+      );
+      _controller = FleatherController(document: doc);
+    } catch (err, st) {
+      if (kDebugMode) {
+        print('Cannot read welcome.json: $err\n$st');
+      }
+      _controller = FleatherController();
+    }
+    setState(() {});
   }
 // //  Person currentUser = ref.read(nestAuthProvider.notifier).currentUser;
 // //     void tap2clipboard(String text) =>
@@ -103,6 +148,31 @@ class _HtmlTextEditorState extends ConsumerState<HtmlTextEditor> {
         appBar: AppBar(
         title: Text('Message Template Editor'), 
       ),
+            floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final picker = ImagePicker();
+          final image = await picker.pickImage(source: ImageSource.gallery);
+          if (image != null) {
+            final selection = _controller!.selection;
+            _controller!.replaceText(
+              selection.baseOffset,
+              selection.extentOffset - selection.baseOffset,
+              EmbeddableObject('image', inline: false, data: {
+                'source_type': kIsWeb ? 'url' : 'file',
+                'source': image.path,
+              }),
+            );
+            _controller!.replaceText(
+              selection.baseOffset + 1,
+              0,
+              '\n',
+              selection:
+                  TextSelection.collapsed(offset: selection.baseOffset + 2),
+            );
+          }
+        },
+        child: const Icon(Icons.add_a_photo),
+      ),
         backgroundColor: Colors.white,
         resizeToAvoidBottomInset: true,
         body: Column(
@@ -110,234 +180,90 @@ class _HtmlTextEditorState extends ConsumerState<HtmlTextEditor> {
             CreateTemplateForm(
               formCode: "template",
             ),
-            // HtmlEditor(
-            //     controller: controller, //required
-            //     htmlEditorOptions: HtmlEditorOptions(
-            //       hint: "Your text here...",
-            //       //initalText: "text content initial, if any",
-            //     ),
-            //     // otherOptions: OtherOptions(
-            //     //   height: 400,
-            //     // ),
-            //     htmlToolbarOptions: HtmlToolbarOptions(defaultToolbarButtons: [
-            //       StyleButtons(),
-            //       FontSettingButtons(),
-            //       FontButtons(),
-            //       ColorButtons(),
-            //       ListButtons(),
-            //       ParagraphButtons(),
-            //       InsertButtons(),
-            //       OtherButtons(),
-            //     ], customToolbarButtons: [
-            //       //your widgets here
-            //       textButton(
-            //           text: 'Clear Editor',
-            //           onPressed: () {
-            //             controller.clear();
-            //           }),
-            //       textButton(
-            //           text: 'Read from Minio',
-            //           onPressed: () {
-            //             loadHtmlFromMinio("TPL_TEST.html");
-            //           }),
-            //                  textButton(
-            //       text: 'Save to Minio',
-            //       onPressed: () {
-            //         saveHtmlToMinio("TPL_TEST.html");
-            //       }),
-            //     ], customToolbarInsertionIndices: [
-            //       2,
-            //       5,
-            //       6
-            //     ]))
-            // ToolBar(
-            //   toolBarColor: _toolbarColor,
-            //   padding: const EdgeInsets.all(8),
-            //   iconSize: 25,
-            //   iconColor: _toolbarIconColor,
-            //   activeIconColor: Colors.greenAccent.shade400,
-            //   controller: controller,
-            //   crossAxisAlignment: WrapCrossAlignment.start,
-            //   direction: Axis.horizontal,
-            //   customButtons: [
-            //     Container(
-            //       width: 25,
-            //       height: 25,
-            //       decoration: BoxDecoration(
-            //           color: _hasFocus ? Colors.green : Colors.grey,
-            //           borderRadius: BorderRadius.circular(15)),
-            //     ),
-            //     InkWell(
-            //         onTap: () => unFocusEditor(),
-            //         child: const Icon(
-            //           Icons.favorite,
-            //           color: Colors.black,
-            //         )),
-            //     InkWell(
-            //         onTap: () async {
-            //           var selectedText = await controller.getSelectedText();
-            //           logNoStack.i('selectedText $selectedText');
-            //           var selectedHtmlText =
-            //               await controller.getSelectedHtmlText();
-            //           logNoStack.i('selectedHtmlText $selectedHtmlText');
-            //         },
-            //         child: const Icon(
-            //           Icons.add_circle,
-            //           color: Colors.black,
-            //         )),
-            //   ],
-            // ),
-            // Expanded(
-            //   child: QuillHtmlEditor(
-            //     text: "<h1>Hello</h1>This is a quill html editor example 😊",
-            //     hintText: 'Hint text goes here',
-            //     controller: controller,
-            //     isEnabled: true,
-            //     ensureVisible: false,
-            //     minHeight: 500,
-            //     autoFocus: false,
-            //     textStyle: _editorTextStyle,
-            //     hintTextStyle: _hintTextStyle,
-            //     hintTextAlign: TextAlign.start,
-            //     padding: const EdgeInsets.only(left: 10, top: 10),
-            //     hintTextPadding: const EdgeInsets.only(left: 20),
-            //     backgroundColor: _backgroundColor,
-            //     inputAction: InputAction.newline,
-            //     onEditingComplete: (s) => logNoStack.i('Editing completed $s'),
-            //     loadingBuilder: (context) {
-            //       return const Center(
-            //           child: CircularProgressIndicator(
-            //         strokeWidth: 1,
-            //         color: Colors.red,
-            //       ));
-            //     },
-            //     onFocusChanged: (focus) {
-            //       logNoStack.i('has focus $focus');
-            //       setState(() {
-            //         _hasFocus = focus;
-            //       });
-            //     },
-            //     onTextChanged: (text) =>
-            //         logNoStack.i('widget text change $text'),
-            //     onEditorCreated: () {
-            //       logNoStack.i('Editor has been loaded');
-            //       setHtmlText('Testing text on load');
-            //     },
-            //     onEditorResized: (height) =>
-            //         logNoStack.i('Editor resized $height'),
-            //     onSelectionChanged: (sel) =>
-            //         logNoStack.i('index ${sel.index}, range ${sel.length}'),
-            //   ),
-            // ),
+           _controller == null
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                FleatherToolbar.basic(
+                    controller: _controller!, editorKey: _editorKey),
+                Divider(height: 1, thickness: 1, color: Colors.grey.shade200),
+                Expanded(
+                  child: FleatherEditor(
+                    controller: _controller!,
+                    focusNode: _focusNode,
+                    editorKey: _editorKey,
+                    padding: EdgeInsets.only(
+                      left: 16,
+                      right: 16,
+                      bottom: MediaQuery.of(context).padding.bottom,
+                    ),
+                    onLaunchUrl: _launchUrl,
+                    maxContentWidth: 800,
+                    embedBuilder: _embedBuilder,
+                    spellCheckConfiguration: SpellCheckConfiguration(
+                        spellCheckService: DefaultSpellCheckService(),
+                        misspelledSelectionColor: Colors.red,
+                        misspelledTextStyle:
+                            DefaultTextStyle.of(context).style),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
-        // bottomNavigationBar: Container(
-        //   width: double.maxFinite,
-        //   color: _toolbarColor,
-        //   padding: const EdgeInsets.all(8),
-        //   child: Wrap(
-        //     children: [
-        //       textButton(
-        //           text: 'Save to Minio',
-        //           onPressed: () {
-        //             saveHtmlToMinio("TPL_TEST.html");
-        //           }),
-        //       textButton(
-        //           text: 'Read from Minio',
-        //           onPressed: () {
-        //             loadHtmlFromMinio("TPL_TEST.html");
-        //           }),
-        //       textButton(
-        //           text: 'Set Text',
-        //           onPressed: () {
-        //             setHtmlText('This text is set by you 🫵');
-        //           }),
-        //       textButton(
-        //           text: 'Get Text',
-        //           onPressed: () {
-        //             getHtmlText();
-        //           }),
-        //       textButton(
-        //           text: 'Insert Video',
-        //           onPressed: () {
-        //             ////insert
-        //             insertVideoURL(
-        //                 'https://www.youtube.com/watch?v=4AoFA19gbLo');
-        //             insertVideoURL('https://vimeo.com/440421754');
-        //             insertVideoURL(
-        //                 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
-        //           }),
-        //       textButton(
-        //           text: 'Insert Image',
-        //           onPressed: () {
-        //             insertNetworkImage('https://i.imgur.com/0DVAOec.gif');
-        //           }),
-        //       textButton(
-        //           text: 'Insert Index',
-        //           onPressed: () {
-        //             insertHtmlText("This text is set by the insertText method",
-        //                 index: 10);
-        //           }),
-        //       textButton(
-        //           text: 'Undo',
-        //           onPressed: () {
-        //             controller.undo();
-        //           }),
-        //       textButton(
-        //           text: 'Redo',
-        //           onPressed: () {
-        //             controller.redo();
-        //           }),
-        //       textButton(
-        //           text: 'Clear History',
-        //           onPressed: () async {
-        //             controller.clearHistory();
-        //           }),
-        //       textButton(
-        //           text: 'Clear Editor',
-        //           onPressed: () {
-        //             controller.clear();
-        //           }),
-        //       textButton(
-        //           text: 'Get Delta',
-        //           onPressed: () async {
-        //             var delta = await controller.getDelta();
-        //             debugPrint('delta');
-        //             debugPrint(jsonEncode(delta));
-        //           }),
-        //       textButton(
-        //           text: 'Set Delta',
-        //           onPressed: () {
-        //             final Map<dynamic, dynamic> deltaMap = {
-        //               "ops": [
-        //                 {
-        //                   "insert": {
-        //                     "video":
-        //                         "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-        //                   }
-        //                 },
-        //                 {
-        //                   "insert": {
-        //                     "video": "https://www.youtube.com/embed/4AoFA19gbLo"
-        //                   }
-        //                 },
-        //                 {"insert": "Hello"},
-        //                 {
-        //                   "attributes": {"header": 1},
-        //                   "insert": "\n"
-        //                 },
-        //                 {"insert": "You just set the Delta text 😊\n"}
-        //               ]
-        //             };
-        //             controller.setDelta(deltaMap);
-        //           }),
-        //    ],
-        //  ),
+        
       ),
     );
     // );
   }
 
+Widget _embedBuilder(BuildContext context, EmbedNode node) {
+    if (node.value.type == 'icon') {
+      final data = node.value.data;
+      // Icons.rocket_launch_outlined
+      return Icon(
+        IconData(int.parse(data['codePoint']), fontFamily: data['fontFamily']),
+        color: Color(int.parse(data['color'])),
+        size: 18,
+      );
+    }
+
+    if (node.value.type == 'image') {
+      final sourceType = node.value.data['source_type'];
+      ImageProvider? image;
+      if (sourceType == 'assets') {
+        image = AssetImage(node.value.data['source']);
+      } else if (sourceType == 'file') {
+        image = FileImage(File(node.value.data['source']));
+      } else if (sourceType == 'url') {
+        image = NetworkImage(node.value.data['source']);
+      }
+      if (image != null) {
+        return Padding(
+          // Caret takes 2 pixels, hence not symmetric padding values.
+          padding: const EdgeInsets.only(left: 4, right: 2, top: 2, bottom: 2),
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: image, fit: BoxFit.cover),
+            ),
+          ),
+        );
+      }
+    }
+
+    return defaultFleatherEmbedBuilder(context, node);
+  }
+
+  void _launchUrl(String? url) async {
+    if (url == null) return;
+    final uri = Uri.parse(url);
+    // final canLaunch = await canLaunchUrl(uri);
+    // if (canLaunch) {
+    //   await launchUrl(uri);
+    // }
+  }
   Widget textButton({required String text, required VoidCallback onPressed}) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -588,5 +514,45 @@ class _HtmlTextEditorState extends ConsumerState<HtmlTextEditor> {
       encoding: Encoding.getByName('utf-8'),
     );
     return response.body;
+  }
+}
+
+/// This is an example insert rule that will insert a new line before and
+/// after inline image embed.
+class ForceNewlineForInsertsAroundInlineImageRule extends InsertRule {
+  @override
+  Delta? apply(Delta document, int index, Object data) {
+    if (data is! String) return null;
+
+    final iter = DeltaIterator(document);
+    final previous = iter.skip(index);
+    final target = iter.next();
+    final cursorBeforeInlineEmbed = _isInlineImage(target.data);
+    final cursorAfterInlineEmbed =
+        previous != null && _isInlineImage(previous.data);
+
+    if (cursorBeforeInlineEmbed || cursorAfterInlineEmbed) {
+      final delta = Delta()..retain(index);
+      if (cursorAfterInlineEmbed && !data.startsWith('\n')) {
+        delta.insert('\n');
+      }
+      delta.insert(data);
+      if (cursorBeforeInlineEmbed && !data.endsWith('\n')) {
+        delta.insert('\n');
+      }
+      return delta;
+    }
+    return null;
+  }
+
+  bool _isInlineImage(Object data) {
+    if (data is EmbeddableObject) {
+      return data.type == 'image' && data.inline;
+    }
+    if (data is Map) {
+      return data[EmbeddableObject.kTypeKey] == 'image' &&
+          data[EmbeddableObject.kInlineKey];
+    }
+    return false;
   }
 }
