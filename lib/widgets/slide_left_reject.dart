ea3abf0437b3a04132a102d7948e284c147e6d85
Adam Crow <acrow@crowtech.com.au>
2025-05-28 22:24:10 +1000

    Updated nest

---
+++ lib/widgets/slide_left_reject.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:notifi/i18n/strings.g.dart' as nt;
+
+Widget slideLeftBackground() {
+  return Container(
+    color: Colors.red,
+    child: Align(
+      alignment: Alignment.centerRight,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          const Icon(
+            Icons.delete,
+            color:  Color.fromARGB(255, 6, 5, 5),
+          ),
+          Text(
+            " ${nt.t.invite.reject}",
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.right,
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+    ),
+  );
+}
---
+++ lib/widgets/slide_right_approve.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:notifi/i18n/strings.g.dart' as nt;
+
+Widget slideRightBackground() {
+  return Container(
+    color: Colors.green,
+    child: Align(
+       alignment: Alignment.centerLeft,
+      child: Row(
+        
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(
+            width: 20,
+          ),
+          const Icon(
+            Icons.edit,
+            color: Colors.white,
+          ),
+          Text(
+            " ${nt.t.invite.approve}",
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ],
+      ),
+     
+    ),
+  );
+}
