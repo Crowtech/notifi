29da6262f1325f44c037ffa57f8f03f2e7f463bc
Adam Crow <acrow@crowtech.com.au>
2025-06-09 14:50:15 +1000

    Updated nest

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 494 (247 per locale)
 ///
-/// Built on 2025-06-09 at 04:42 UTC
+/// Built on 2025-06-09 at 04:50 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
