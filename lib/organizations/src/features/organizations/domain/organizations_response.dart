a45c70ca0de14e9541ef7c1f4bcebd2eafefbe18
Adam Crow <acrow@crowtech.com.au>
2025-06-25 17:04:47 +1000

    Updated totalItems

--- lib/organizations/src/features/organizations/domain/organizations_response.dart
+++ lib/organizations/src/features/organizations/domain/organizations_response.dart
@@ -8,58 +8,25 @@ part 'organizations_response.g.dart';
 
 @freezed
 @JsonSerializable()
-sealed class OrganizationsResponse with _$OrganizationsResponse {
+class OrganizationsResponse with _$OrganizationsResponse {
   OrganizationsResponse({
-    @JsonKey(name: 'startIndex') required this.startIndex,
-    @JsonKey(name: 'items') required this.items,
-    @JsonKey(name: 'resultCount') required this.resultCount,
+    @JsonKey(name: 'startIndex') required int startIndex,
+    @JsonKey(name: 'items') required List<Organization> items,
+    @JsonKey(name: 'resultCount') required int resultCount,
     @JsonKey(name: 'totalItems') required int totalItems,
-
   });
 
   @override
   int startIndex=0;
   @override
-  List<Organization> items = [];
+  List<Organization> items=[];
   @override
   int resultCount=0;
-    @override
+  @override
   int totalItems=0;
 
-  factory OrganizationsResponse.fromJson(Map<String, Object?> json)
-      => _$OrganizationsResponseFromJson(json);
+  factory OrganizationsResponse.fromJson(Map<String, Object?> json) =>
+      _$OrganizationsResponseFromJson(json);
 
   Map<String, Object?> toJson() => _$OrganizationsResponseToJson(this);
 }
-
-// @freezed
-// sealed class OrganizationsResponse with _$OrganizationsResponse {
-//    @JsonSerializable(explicitToJson: true)
-//   OrganizationsResponse({
-//     @JsonKey(name: 'startIndex') required this.page,
-//     @JsonKey(name: 'items') required this.results,
-//     @JsonKey(name: 'resultCount') required this.totalItems,
-//     @JsonKey(name: 'totalItems') required this.totalPages,
-//   }) ;
-
-//   final int page;
-//   final List<Organization> results;
-//   final int totalItems;
-//   final int totalPages;
-
-
-// factory OrganizationsResponse.fromJson(Map<String, dynamic> json) => _$OrganizationsResponseFromJson(json);
-
-// // extension NOrganizationsResponseX on OrganizationsResponse {
-// //   //@late
-// //   bool get isEmpty => !hasResults();
-
-// //   bool hasResults() {
-// //     return results.isNotEmpty;
-// //   }
-
-// //   bool hasErrors() {
-// //     return errors.isNotEmpty;
-// //   }
-// // }
-// }
