e789b31cf733c17a019c950103b96322fdbc3fe9
Adam Crow <acrow@crowtech.com.au>
2025-06-25 18:21:10 +1000

    Updated totalItems

--- lib/persons/src/features/persons/domain/npersons_response.dart
+++ lib/persons/src/features/persons/domain/npersons_response.dart
@@ -6,9 +6,6 @@ import 'nperson.dart';
 part 'npersons_response.freezed.dart';
 part 'npersons_response.g.dart';
 
-//{created: 2025-03-12T06:36:41.331630375, startIndex: 0, items: [{id: 1, created: 2025-01-08T12:29:22.045304, updated: 2025-03-10T23:56:09.999705, code: ORG_PANTA, name: Panta, orgId: 2, description: Panta,
-//avatarUrl: ORG_PANTA-avatar-2025-03-09T16:40:51.725539.png, orgType: org, url: https://www.pantagroup.org}, {id: 10251, created: 2025-03-06T01:53:08.333173, updated: 2025-03-10T23:56:09.987446, code: ORG_CROWTECH, name: Crowtech, orgId: 2, description: Crowtech,
-//avatarUrl: ORG_CROWTECH-avatar-2025-03-09T16:40:51.725539.png, orgType: org, url: https://crowtech.com.au}], totalItems: 2, processingTime: 9414451}
 
 @freezed
 @JsonSerializable()
